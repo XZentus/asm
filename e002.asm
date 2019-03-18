75eff4af5958105c6402b7fc913cc09d88bece28
XZentus <z_xmn@mail.ru>
2019-03-18 17:30:21 +0300

    Update e002.asm

--- e002.asm
+++ e002.asm
@@ -5,82 +5,125 @@ entry start
 section '.text' readable executable code
 
 start:
-        mov eax, 1
-        mov ebx, 1
-        xor ecx, ecx
 
-loopiter:
+        sub     rsp, 8*5
+        mov     rdi, 3          ; num
+        mov     rbx, 0          ; acc
 
-        add ebx, eax
-        add eax, ebx
+        iterloop:
+                call    isDiv3_5
+                test    al, al
+                jz      itercont
+                add     rbx, rdi
+        itercont:
+                inc     rdi
+                cmp     rdi, 1000
+                jb      iterloop
 
-        xor edx, edx
-        xor r8d, r8d
-        test eax, 1
-        cmove edx, eax
-        add ecx, edx
+        mov     rcx, rbx
+        lea     rdx, [_result1 - 1]
+        call    print10Base
 
-        test ebx, 1
-        cmove r8d, ebx
-        add ecx, r8d
-
-        cmp eax, 4000000
-        jb loopiter
-
-        lea rdx, [_label - 2]
-        call print10base
-
-
-        sub rsp, 8*5         ; reserve stack for API use and make stack dqword aligned
-
-
-        mov     r9d, 0
-        lea     r8,  [_caption]
+        mov     r9,  0
+        lea     r8,  [_caption1]
         lea     rdx, [_message]
         mov     rcx, 0
         call    [MessageBoxA]
 
-        mov     ecx,eax
+        mov     rcx, rax
         call    [ExitProcess]
 
-print10base:           ;print10Base(rcx: unsigned number, rdx: ptr_back)
 
-        mov rbx, rdx ; rbx: ptr
-        mov rax, rcx ; rax: number
-        mov r10, 10
-
-p10loop:
-        xor rdx, rdx
-        div r10
-        add dl, '0'
-        mov [rbx], dl
-;        mov [rbx], rdx + 30
-        dec rbx
-        cmp rax, 0
-        jne p10loop
+print10Base:
+        mov     rdi, rcx
+        mov     rsi, rdx
+        mov     r8,  0xCCCCCCCCCCCCCCCD
+        p10loop:
+                mov     rax, rdi
+                mul     r8
+                shr     rdx, 3
+                lea     eax, [rdx + rdx]
+                lea     eax, [rax + 4*rax]
+                mov     ecx, edi
+                sub     ecx, eax
+                or      cl, 48
+                mov     [rsi], cl
+;                add     rsi, -1
+                dec     rsi
+                cmp     rdi, 9
+                mov     rdi, rdx
+                ja      p10loop
         ret
+
+
+isDiv3_5:
+        mov     rcx, 0xAAAAAAAAAAAAAAAB
+        mov     rax, rdi
+        mul     rcx
+        shr     rdx, 1
+        lea     rax, [rdx + 2*rdx]
+        cmp     rdi, rax
+        je      .LBB0_1
+        mov     rcx, 0xCCCCCCCCCCCCCCCD
+        mov     rax, rdi
+        mul     rcx
+        shr     rdx, 2
+        lea     rax, [rdx + 4*rdx]
+        cmp     rdi, rax
+        sete    al
+        ret
+.LBB0_1:
+        mov     al, 1
+        ret
+
+
+next_div:
+        mov     rax, rcx
+        mov     r9,  rdx
+        xor     rdx, rdx
+        div     r9
+        test    rdx, rdx
+        mov     rax, rcx
+        jz      nd_divisor
+        add     rax, r9
+        sub     rax, rdx
+nd_divisor:
+        ret
+
+prev_div:
+        mov     rax, rcx
+        mov     r9,  rdx
+        xor     rdx, rdx
+        div     r9
+        mov     rax, rcx
+        sub     rax, rdx
+        ret
+
 
 section '.idata' import data readable writable
 
-  dd 0,0,0,RVA kernel_name,RVA kernel_table
-  dd 0,0,0,RVA user_name,RVA user_table
-  dd 0,0,0,0,0
+        dd 0,0,0, RVA kernel_name, RVA kernel_table
+        dd 0,0,0, RVA user_name, RVA user_table
+        dd 0,0,0,0,0
 
-  kernel_table:
-    ExitProcess dq RVA _ExitProcess
-    dq 0
-  user_table:
-    MessageBoxA dq RVA _MessageBoxA
-    dq 0
+        kernel_table:
+                ExitProcess dq RVA _ExitProcess
+                dq 0
+        user_table:
+                MessageBoxA dq RVA _MessageBoxA
+                dq 0
 
-  kernel_name db 'KERNEL32.DLL',0
-  user_name db 'USER32.DLL',0
+        kernel_name db 'KERNEL32.DLL',0
+        user_name   db 'USER32.DLL',0
 
-  _ExitProcess dw 0
-    db 'ExitProcess',0
-  _MessageBoxA dw 0
-    db 'MessageBoxA',0
+        _ExitProcess dw 0
+                     db 'ExitProcess',0
+        _MessageBoxA dw 0
+                     db 'MessageBoxA',0
 
-  _caption        db 'Result', 0
-  _message        db '          ', 0
-  _label          db ?
+        _caption1    db 'Result',0
+        _caption2    db 'Result: analytic',0
+        _message     db '                    '
+_result1:
+                     db 13, 10, '                    '
+        _result2     db 0
