45d78e0890c63f2a61c9a069332b5ea75391a036
XZentus <z_xmn@mail.ru>
2019-03-21 00:09:53 +0300

    Update e001.asm

--- e001.asm
+++ e001.asm
@@ -4,28 +4,41 @@ entry start
 
 section '.text' readable executable code
 
+upper_limit = 1000000000
+
 start:
 
         sub     rsp, 8*5
         mov     rdi, 3          ; num
         mov     rbx, 0          ; acc
 
-        iterloop:
+        .iterloop:
                 call    isDiv3_5
                 test    al, al
-                jz      itercont
+                jz      .itercont
                 add     rbx, rdi
-        itercont:
+        .itercont:
                 inc     rdi
-                cmp     rdi, 1000
-                jb      iterloop
+                cmp     rdi, upper_limit
+                jb      .iterloop
 
         mov     rcx, rbx
+
         lea     rdx, [_result1 - 1]
         call    print10Base
 
+;       -------------------
+        xor     rcx, rcx
+        mov     rdx, upper_limit
+        mov     r9,  5
+        call    sum_range
+        mov     rcx, rax
+
+        lea     rdx, [_result2 - 1]
+        call    print10Base
+;       -------------------
         mov     r9,  0
-        lea     r8,  [_caption1]
+        lea     r8,  [_caption]
         lea     rdx, [_message]
         mov     rcx, 0
         call    [MessageBoxA]
@@ -38,7 +51,7 @@ print10Base:
         mov     rdi, rcx
         mov     rsi, rdx
         mov     r8,  0xCCCCCCCCCCCCCCCD
-        p10loop:
+        .p10loop:
                 mov     rax, rdi
                 mul     r8
                 shr     rdx, 3
@@ -48,15 +61,14 @@ print10Base:
                 sub     ecx, eax
                 or      cl, 48
                 mov     [rsi], cl
-;                add     rsi, -1
                 dec     rsi
                 cmp     rdi, 9
                 mov     rdi, rdx
-                ja      p10loop
+                ja      .p10loop
         ret
 
 
-isDiv3_5:
+isDiv3_5:                                       ; rdi%3 == 0 || rdi%5 == 0
         mov     rcx, 0xAAAAAAAAAAAAAAAB
         mov     rax, rdi
         mul     rcx
@@ -72,31 +84,66 @@ isDiv3_5:
         cmp     rdi, rax
         sete    al
         ret
-.LBB0_1:
+        .LBB0_1:
         mov     al, 1
         ret
 
 
-next_div:
+next_div:                       ; int: rcx, div: r9
         mov     rax, rcx
-        mov     r9,  rdx
+;        mov     r9,  rdx
         xor     rdx, rdx
         div     r9
         test    rdx, rdx
         mov     rax, rcx
-        jz      nd_divisor
+        jz      .nd_divisor
         add     rax, r9
         sub     rax, rdx
-nd_divisor:
+        .nd_divisor:
         ret
 
-prev_div:
+prev_div:                       ; int: rcx, div: r9
         mov     rax, rcx
-        mov     r9,  rdx
+;        mov     r9,  rdx
         xor     rdx, rdx
         div     r9
         mov     rax, rcx
         sub     rax, rdx
+        ret
+
+n_divs:                         ; from: rcx, to: rdx, div: r9
+        mov     rax, rdx
+        sub     rax, rcx
+        xor     rdx, rdx
+        div     r9
+        inc     rax
+        ret
+
+sum_range:                      ; from: rcx, to: rdx, div: r9
+        mov     rdi, rdx
+        call    next_div
+        mov     rsi, rax        ; from: rsi
+        mov     rcx, rdi
+        call    prev_div
+        mov     rdi, rax        ; to: rdi
+
+        mov     rcx, rsi
+        mov     rdx, rdi
+        call    n_divs
+        test    rax, 1
+        jz      .even_divs
+        ; odd_divs
+                lea     r10, [rsi + rdi]
+                mul     r10
+                shr     rax, 1
+                ret
+        .even_divs:
+                dec     rax
+                lea     r10, [rsi + rdi]
+                add     r10, r9
+                mul     r10
+                shr     rax, 1
+                add     rax, rsi
         ret
 
 
@@ -121,9 +168,10 @@ section '.idata' import data readable writable
         _MessageBoxA dw 0
                      db 'MessageBoxA',0
 
-        _caption1    db 'Result',0
-        _caption2    db 'Result: analytic',0
-        _message     db '                    '
+buf_len = 20
+
+        _caption    db 'Result',0
+        _message:   db buf_len dup ' '
 _result1:
-                     db 13, 10, '                    '
+                     db 13, 10, buf_len dup ' '
         _result2     db 0
